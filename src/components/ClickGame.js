9cd66c2b4c50ae5acf7be67abfd7ebc36741f89a
Daniel Rocha <x00170715@mytudublin.ie>
2022-04-20 02:28:42 +0100

    clickgame updated

--- src/components/ClickGame.js
+++ src/components/ClickGame.js
@@ -2,28 +2,33 @@ import React, { Component } from 'react';
 
 
 class ClickGame extends Component{
-
-    state = {
-        numWin: 8,
-        num: 0,
-        isWinner: false
-    }
-
-    numGenerator = () => {
-        let randNum = Math.floor(Math.random() * 10);
+    constructor(props){
+        super(props);
+        this.state={
+                numWin: 8,
+                num: 0,
+                isWinner: false
+        }
+        this.numGenerator = this.numGenerator.bind(this);
+    }   
+  
+    numGenerator(){
+        let randNum = Math.floor(Math.random() * 10) + 1;
+        console.log(randNum);
+        if(randNum === 7){
+            this.setState({isWinner : true});
+        }
         this.setState({num: randNum});
     };
 
     render(){
 
-        if(this.state.num === this.state.numWin){
-            this.setState({isWinner: true})
-        }
-
         return(
             <div>
-                <h1>Number is {this.state.num}</h1>
-                {!this.state.isWinner ? <button onClick={this.numGenerator}>Click Me</button> : <h2>You WIN!!</h2>}
+                <h1>Number is: {this.state.num}</h1>
+                {/* {!this.state.isWinner ? <button onClick={this.numGenerator}>Generate Random Number</button> : <h2>You WIN!!</h2>} */}
+                {/* {this.state.num === 7 ? <h2>YOU WIN!!</h2> : <button onClick={this.numGenerator}>Generate Random Number</button>} */}
+                {this.state.isWinner ? <h2>YOU WIN!!</h2> : <button onClick={this.numGenerator}>Generate Random Number</button>}
             </div>
         )
     }
